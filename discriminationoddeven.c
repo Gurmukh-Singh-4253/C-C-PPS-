cb4adf7fc0a327cd16f8eaa6594eaf50866b2b68
Gurmukh Singh <111715777+Gurmukh-Singh-4253@users.noreply.github.com>
2023-12-05 05:37:46 +0530

    main

--- discriminationoddeven.c
+++ discriminationoddeven.c
@@ -4,14 +4,8 @@ int main(){
   int input, even[10], odd[10],evencount = 0, oddcount = 0;
   for(int i = 0; i<10; i++){
     scanf("%d",&input);
-    if (input%2==0){
-      even[evencount]=input;
-      evencount++;
-    }
-    else{
-      odd[oddcount]=input;
-      oddcount++;
-    }
+    if (input%2==0) even[evencount++]=input;
+    else odd[oddcount++]=input;
   }
   printf("Even integers: ");
   for(int i = 0; i<evencount ; i++){
